b16f07729895afcd5755c75de98ff0b2fda2ebe7
Britannf <sepakat952@gmail.com>
2026-01-28 09:48:26 +0700

    feat: implement simple router

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controllers;
+
+class StudentController
+{
+    
+    public function index():void 
+    {
+        echo '<h1> Daftar Siswa</h1>';
+        echo '<p> Menampilkan Daftar Siswa</p>';
+    }
+
+    public function create():void
+    {
+        echo '<h1> Tambah Siswa</h1>';
+        echo '<p> Menampilkan Form Tambah Siswa</p>';
+    }
+}
+?>
