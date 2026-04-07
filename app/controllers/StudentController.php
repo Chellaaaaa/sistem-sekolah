e4d7bcf2622163020ebf9b424c28f0c85df8fb8f
Britannf <sepakat952@gmail.com>
2026-04-07 14:35:54 +0700

    feat: implement student detail and edit page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -17,6 +17,11 @@ class StudentController
     {
         require_once '../app/views/students/show.php';
     }
+
+    public function edit(string $id)
+    {
+        require_once '../app/views/students/edit.php';
+    }
 }
 
 ?>
