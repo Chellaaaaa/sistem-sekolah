5b1c95ccf413327513b78e1ae22cd97621c74aa6
Britannf <sepakat952@gmail.com>
2026-04-08 10:13:38 +0700

    feat: implement layout app

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -1,72 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daftar Siswa</title>
-    <link rel="stylesheet" href="/css/output.css">
-</head>
-<body class="flex flex-col min-h-screen bg-gray-100">
-    <!-- Header Start -->
-     <header class="bg-blue-900 text-white">
-        <div class="flex items-center justify-between container mx-auto p-4">
-            <a class="font-bold" href="/students">Sistem Sekolah</a>
-            <a class="py-2 px-4 bg-white text-blue-900 rounded-lg" href="/students/create">+ Tambah Siswa</a>
-        </div>
-     </header>
-    <!-- Header End-->
+<div class="mt-8 space-y-4">
+    <!-- Card Header Start -->
+    <div class="bg-white shadow p-4 rounded-lg">
+        <h1 class="font-bold text-2xl">Daftar Siswa</h1>
+        <p>Menampilkan daftar siswa yang terdaftar</p>
+    </div>
+    <!-- Card Header End -->
 
-    <!-- Main Start -->
-     <main class="grow container mx-auto">
-        <div class="mt-8 space-y-4">
-            <!-- Card Header Start -->
-             <div class="bg-white shadow p-4 rounded-lg">
-                <h1 class="font-bold text-2xl">Daftar Siswa</h1>
-                <p>Menampilkan daftar siswa yang terdaftar</p>
-             </div>
-            <!-- Card Header End -->
+    <!-- Card Content Start -->
+    <div class="bg-white rounded-lg shadow">
+        <table class="w-full">
+            <thead class="bg-gray-200">
+                <tr>
+                    <th class="px-4 py-2 text-left">No</th>
+                    <th class="px-4 py-2 text-left">Nama</th>
+                    <th class="px-4 py-2 text-left">Kelas</th>
+                    <th class="px-4 py-2 text-left">NIS</th>
+                    <th class="px-4 py-2 text-left">No Telepon</th>
+                    <th class="px-4 py-2">Aksi</th>
+                </tr>
+            </thead>
 
-            <!-- Card Content Start -->
-             <div class="bg-white rounded-lg shadow">
-                <table class="w-full">
-                    <thead class="bg-gray-200">
-                        <tr>
-                            <th class="px-4 py-2 text-left">No</th>
-                            <th class="px-4 py-2 text-left">Nama</th>
-                            <th class="px-4 py-2 text-left">Kelas</th>
-                            <th class="px-4 py-2 text-left">NIS</th>
-                            <th class="px-4 py-2 text-left">No Telepon</th>
-                            <th class="px-4 py-2">Aksi</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <tr>
-                            <td class="px-4 py-2 text-left">1</td>
-                            <td class="px-4 py-2 text-left">Britania</td>
-                            <td class="px-4 py-2 text-left">XI TKJ 1</td>
-                            <td class="px-4 py-2 text-left">01234</td>
-                            <td class="px-4 py-2 text-left">09991234556</td>
-                            <td class="px-4 py-2">
-                                <div class="flex justify-center items-center gap-4">
-                                    <a href="" class="text-green-500">Detail</a>
-                                    <a href="" class="text-yellow-500">Edit</a>
-                                    <a href="" class="text-red-500">Hapus</a>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-             </div>
-            <!-- Card Content End -->
-        </div>
-     </main>
-    <!-- Main End -->
-
-    <!-- Footer Start-->
-     <footer class="bg-gray-700 text-white p-4 text-center">
-        2026 - SMK Kristen Immanuel Pontianak - Sistem Sekolah
-     </footer>
-    <!-- Footer End -->
-</body>
-</html>
+            <tbody>
+                <tr>
+                    <td class="px-4 py-2 text-left">1</td>
+                    <td class="px-4 py-2 text-left">Britania</td>
+                    <td class="px-4 py-2 text-left">XI TKJ 1</td>
+                    <td class="px-4 py-2 text-left">01234</td>
+                    <td class="px-4 py-2 text-left">09991234556</td>
+                    <td class="px-4 py-2">
+                        <div class="flex justify-center items-center gap-4">
+                            <a href="" class="text-green-500">Detail</a>
+                            <a href="" class="text-yellow-500">Edit</a>
+                            <a href="" class="text-red-500">Hapus</a>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- Card Content End -->
+</div>
